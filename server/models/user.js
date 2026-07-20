2cfa89468fb26579c2f70e8c0e2d5134cfa5d4aa
GautamiBendre <gautamibendre23@gmail.com>
2026-07-20 19:57:46 +0530

    created user model with authentication fields

---
+++ server/models/user.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {   name:{
+        type :String,
+        required :true,
+        trim:true
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+        },
+        phoneNumber: {
+            type: String, //Phone numbers are identifiers, not values you perform calculations on.
+            required: true,
+            trim: true,
+            unique: true,
+        },
+
+        password: {
+            type: String,
+            required: true,
+        },
+
+        reminderEnabled: {
+            type: Boolean,
+            default: true,
+        },
+    },
+    {
+        timestamps:true, //instead of manualy storing createdAt ,updatedAt 
+    }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
